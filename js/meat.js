e3cd31938c1a36054717c58378f09c1880aa13c5
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-04-27 15:29:11 -0500

    meat.js had old version?

--- js/meat.js
+++ js/meat.js
@@ -2,11 +2,20 @@
 var SandwichMaker = (function(maker) {
 
   // Private variable to store the different meat prices
-  var meatPrices = {"turkey": 0.90, "bacon": 1.50};
-
+  var meatPrices = {"bacon": 0.90, "turkey": 1.50};
+  
   // Augment the original object with another method
-  maker.addMeat = function() {
-    return ???;
+  maker.addMeat = function(Topping) {
+  	console.log("selectedtopping from meat.js", Topping);
+  	var meatPrice = 0;
+  	console.log("meatPrices", meatPrices);
+  	for (i in meatPrices){
+  		if(Topping==i){
+  			meatPrice=meatPrices[i];
+  			console.log("meatPrice", meatPrice);
+  		}
+  	}
+    return meatPrice;
   };
 
   // Return the new, augmented object with the new method on it
